6b4aef6d7eb6393470c9277994ec03eef81d84cc
Maria Vorobeva <tirimaria@gmail.com>
2026-01-28 19:15:14 +0000

    Configure proxy requests to backend server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // proxy request to backend server
+  // source: https://stackoverflow.com/questions/60925133/proxy-to-backend-with-default-next-js-dev-server */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
